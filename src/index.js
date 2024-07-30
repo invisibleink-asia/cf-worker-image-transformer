99ea1e65a9abda05123b8a77af83382816f22f11
Jay <me@jirayu.in.th>
2024-07-30 13:14:36 +0700

    return original request if not image

--- src/index.js
+++ src/index.js
@@ -45,6 +45,10 @@ async function handleRequest(request, env) {
     let pathFragments = url.pathname.split('/');
     let filename = String( pathFragments.slice(-1) );
 
+    if (!/\.(jpg|jpeg|png|gif|webp|ico)$/i.test(url.pathname)) {
+      return request;
+    }
+
     let regex = /(-([0-9]*)x([0-9]*)).[A-z]*$/g;
     let dimension = regex.exec(filename);
     let realFilename = '';
@@ -76,7 +80,6 @@ async function handleRequest(request, env) {
       options.cf.image.height = dimension[3];
     }
 
-    // Your Worker is responsible for automatic format negotiation. Check the Accept header.
     const accept = request.headers.get("Accept");
 
     if (/image\/avif/.test(accept)) {
@@ -85,32 +88,12 @@ async function handleRequest(request, env) {
       options.cf.image.format = 'webp';
     }
 
-    // Get URL of the original (full size) image to resize.
-    // You could adjust the URL here, e.g., prefix it with a fixed address of your server,
-    // so that user-visible URLs are shorter and cleaner.
     imageURL = url.toString();
-
-    try {
-      // TODO: Customize validation logic
-      const { pathname } = new URL(imageURL);
-
-      // Optionally, only allow URLs with JPEG, PNG, GIF, or WebP file extensions
-      // @see https://developers.cloudflare.com/images/url-format#supported-formats-and-limitations
-
-      if (!/\.(jpg|jpeg|png|gif|webp|ico)$/i.test(pathname)) {
-        // return new Response('Disallowed file extension', { status: 400 })
-        return request;
-      }
-    } catch (err) {
-      return new Response('Invalid "image" value', { status: 404 });
-    }
   }
 
-  // Build a request that passes through request headers
   const imageRequest = new Request(imageURL, {
     headers: request.headers
   })
 
-  // Returning fetch() with resizing options will pass through response with the resized image.
   return fetch(imageRequest, options);
 }
